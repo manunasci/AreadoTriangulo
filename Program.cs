1f0890469dfe63662522796a34301100274d4920
manunasci <nascimentomanuella73@gmail.com>
2021-09-17 21:25:24 -0300

    Adiciona o programa que faz o calculo da area do triangulo retangulo

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AreadoTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a base do triangulo retângulo: ");
+            string valordaBase = Console.ReadLine();
+            double numerodaBase = Double.Parse(valordaBase);
+
+            Console.WriteLine();
+
+            Console.Write("Digite a altura do triangulo retângulo:");
+            string valordaAltura = Console.ReadLine();
+            double numerodaAltura = Double.Parse(valordaAltura);
+
+            double valordaArea = (numerodaBase*numerodaAltura)/2;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Área do Triangulo Retângulo é: " + valordaArea);
+        }
+    }
+}
